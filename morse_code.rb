d4ee20a1c6f9cdb7fe9cf8e954ee811c16a9938b
Sunga Thawethe <sungabt@gmail.com>
2023-07-28 09:00:33 +0100

    Decode char and string

---
+++ morse_code.rb
@@ -0,0 +1,23 @@
+# print "Hello World"
+
+def decode_char(morse)
+  morse_code = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+morse_code[morse]
+
+end
+
+def decode_string(str)
+  str.split(' ')
+  .map do |word|
+    decode_char word
+  end
+  .join('')
+end
+
